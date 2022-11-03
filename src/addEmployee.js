e3ef2fd1f5cd9adee2d170d4a39192ce6ca9d18a
Gabriel Silper <2019003726@ifam.edu.br>
2022-11-03 16:39:19 -0400

    implementei a função addEmployee, falta o teste

--- src/addEmployee.js
+++ src/addEmployee.js
@@ -1,12 +1,35 @@
 const { company, Employee } = require("./data");
+const {employees} = company;
 
-function addEmployee(employees, ...info) {
+function verifyFunctions(...info) {
     if (info[0] === undefined 
         || info[1] === undefined
         || info[2] === undefined) {
-        
+        throw new Error('Missing params to continue - verify if id, first name and last name was add');
     }
+    if ( typeof info[1] !== 'string' 
+        || typeof info[2] !== 'string' ){
+        throw new Error('Missing params to continue - verify if first name and last name was filled with words');
+    }
+    if ( info[1].length < 3 || info[2].length < 3){
+        throw new Error('Missing params to continue - verify if id, first name and last name was filled correctly');
+    }
+}
+
+function generateRandomID() {
+    const part1 = Math.random().toString(16).substring(5);
+    const part2 = Math.random().toString(16).substring(5);
+    const part3 = Math.random().toString(16).substring(5);
+    const part4 = Math.random().toString(16).substring(5);
+    const ID = `${part1}-${part2}-${part3}-${part4}`;
+    return ID;
+}
+
+function addEmployee(employees, ...info) {
+    verifyFunctions(...info);
     employees.push(new Employee (...info));
 }
+
+console.log(employees);
 
 module.exports = addEmployee;
--- src/addGhostEmployees.js
+++ src/addGhostEmployees.js
@@ -1,7 +1,7 @@
 const { company, Employee } = require("./data");
 
 function addGhostEmployees(employees) {
-    employees.push(new Employee ('0001','Gabriel','Pereira'));
+    employees.push(new Employee ('0001','Gabriel','Silper'));
     employees.push(new Employee ('0002','Fulano','Moreira'));
     employees.push(new Employee ('0003','Sicrano','Bezerra'));
     employees.push(new Employee ('0004','Beltrano','Silva'));
